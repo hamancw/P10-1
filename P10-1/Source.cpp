dde00d72eb73f15e4b70fc365c50b3dcf5f61126
Cade Haman <c-haman@onu.edu>
2024-04-17 20:26:19 -0400

    Completed P10-1

--- P10-1/Source.cpp
+++ P10-1/Source.cpp
@@ -1,29 +1,49 @@
 #include <iostream>
+#include <string>
 #include <ctime>
 #include <cstdlib>
 
 using namespace std;
 
-string scramble(string word) {
+string string_scramble(string word) {
+    if (word.length() <= 3)
+        return word;
 
-	int upper = word.size() - 1;
-	int lower = 1;
-	int firstIndex = rand() % (upper - lower + 1) + lower;
-	int secondIndex = rand() % (upper - lower + 1) + lower;
-	char t = word[firstIndex];
+    int len = word.length();
+    int index1, index2;
 
-	word[firstIndex] = word[secondIndex];
-	word[secondIndex] = t;
+    do {
+        index1 = 1 + rand() % (len - 2);
+        index2 = 1 + rand() % (len - 2);
+    } while (index1 == index2);
 
-	return word;
+    char temp = word[index1];
+    word[index1] = word[index2];
+    word[index2] = temp;
+
+    return word;
 }
 
 int main() {
+    srand(time(0));
 
-	srand(time(0));
-	
-	string word;
-	cout << "Enter a string: ";
-	cin >> word;
-	cout << scramble(word);
+    cout << "Enter a line of text: ";
+    string line;
+    getline(cin, line);
+
+    string word = "";
+    for (char c : line) {
+        if (c == ' ' || c == '\t' || c == '\n') {
+            
+            cout << string_scramble(word) << " ";
+            word = "";
+        }
+        else {
+            word += c;
+        }
+    }
+
+    if (!word.empty()) {
+        cout << string_scramble(word);
+    }
 }
